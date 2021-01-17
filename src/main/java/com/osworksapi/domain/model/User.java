78165664485799dd69bd11e48107655a99b1edd0
Jean Xavier <jxs.jean@gmail.com>
2021-01-17 00:11:33 -0300

    Mapping User with JPA.

--- src/main/java/com/osworksapi/domain/model/User.java
+++ src/main/java/com/osworksapi/domain/model/User.java
@@ -5,13 +5,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(max = 60)
     private String name;
+
+    @Email
+    @Size(max = 255)
     private String email;
+
+    @Size(max = 20)
     private String phone;
 }
