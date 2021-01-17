c65811d13b548c2870d10a6f30054fbdbdec50e4
Jean Xavier <jxs.jean@gmail.com>
2021-01-17 00:16:58 -0300

    Implements CommentRepository

---
+++ src/main/java/com/osworksapi/domain/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.osworksapi.domain.repository;
+
+import com.osworksapi.domain.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
